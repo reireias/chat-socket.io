01e858f9deff719ecc81b00865ca42b7aae1f1b7
reireias <reireias@gmail.com>
2020-04-11 22:24:46 +0900

    update theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,9 +58,9 @@ module.exports = {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
